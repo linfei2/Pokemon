4b15d35381668fbac113db34cb7f36a0a75535c0
Dominika Matczynska <dominika.matczynska@gmail.com>
2020-10-21 19:50:14 +0200

    Random pokemon picker and __str__ functions added

--- pokemon.py
+++ pokemon.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Pokemon:
   def __init__(self, name, level, type_, max_health, current_health, is_knocked_out):
     self.name = name
@@ -6,6 +9,10 @@ class Pokemon:
     self.max_health = max_health
     self.current_health = current_health
     self.is_knocked_out = is_knocked_out
+
+  
+  def __str__(self):
+    return f"{self.name}\nCurrent health: {self.current_health}\nLevel: {self.level}\nType: {self.type_}"
     
 
   def lose_health(self, points):
@@ -13,23 +20,23 @@ class Pokemon:
     if hp_after_loss <= 0:
       return self.knock_out()
     else:
+      self.current_health = hp_after_loss
       print(self.name + " has " + str(hp_after_loss) + " health points left")
-      return self.current_health
   
 
   def gain_health(self, points):
     hp_after_gain = self.current_health + points
     if hp_after_gain >= self.max_health:
       print(self.name + " has maximum health: " + str(self.max_health))
-      return self.max_health
+      self.max_health = 100
     else:
+      self.current_health = hp_after_gain
       print(self.name + " now has " + str(hp_after_gain) + " health points")
-      return self.current_health
 
 
   def knock_out(self):
-    if self.current_health <= 0:
-      print(self.name + " knocked out")
+    self.is_knocked_out = True
+    print(self.name + " is knocked out")
 
 
   def attack(self, other_pokemon):
@@ -37,13 +44,16 @@ class Pokemon:
     p1 = poke_dict[self.type_]
     p2 = poke_dict[other_pokemon.type_]
     dif = p1 - p2
-    if dif in [-3, 2, 1]:
-      damage = self.level*2
-    elif dif in [-2, -1, 3]:
-      damage = self.level/2
+    if self.is_knocked_out != True:
+      if dif in [-3, 2, 1]:
+        damage = self.level*2
+      elif dif in [-2, -1, 3]:
+        damage = self.level/2
+      else:
+        damage = self.level
+      return other_pokemon.lose_health(damage)
     else:
-      damage = self.level
-    return other_pokemon.lose_health(damage)
+      print(f"{self.name} is knocked out and cannot attack anymore.")
 
 
 class Trainer(Pokemon):
@@ -52,18 +62,22 @@ class Trainer(Pokemon):
     self.name = name
     self.potions = potions
     self.active_pokemon = active_pokemon
+  
 
-
+  def __str__(self):
+    return "{}\nMy pokemons: {}\nActive pokemon: {}\nNumber of potions: {}".format(self.name, 
+    [p.name for p in self.pokemon_owned], self.active_pokemon.name, self.potions)
+    
+  
   def use_potion(self, potion_num):
     self.potions -= potion_num
-    print(self.name + " uses potion to heal " + self.active_pokemon.name)
-    self.active_pokemon.gain_health(potion_num*3)
-    return self.active_pokemon.current_health
+    print(self.name + " uses " + str(potion_num) + " potion(s) to heal " + self.active_pokemon.name)
+    return self.active_pokemon.gain_health(potion_num*3)
 
 
   def fight_with(self, other_trainer):
     print(self.name + " uses " + self.active_pokemon.name + " to attack " + other_trainer.active_pokemon.name)
-    return self.active_pokemon.attack(other_trainer.active_pokemon)
+    self.active_pokemon.attack(other_trainer.active_pokemon)
 
 
   def switch_pokemon(self):
@@ -72,22 +86,47 @@ class Trainer(Pokemon):
     print("1: " + self.pokemon_owned[0].name)
     print("2: " + self.pokemon_owned[1].name)
     print("3: " + self.pokemon_owned[2].name)
-    choice = int(input("Click 1, 2 or 3 to make your choice: "))
-    self.active_pokemon = self.pokemon_owned[choice-1]
-    print(self.active_pokemon.name + " is now active!")
-
+    number = int(input("Click 1, 2 or 3 to make your choice: "))
+    choice = self.pokemon_owned[number-1] 
+    if choice.is_knocked_out != True: 
+      self.active_pokemon = choice
+      print(self.active_pokemon.name + " is now active!")
+    else:
+      print("This pokemon is knocked out, you cannot use it")
+      self.active_pokemon = self.active_pokemon
+      
 
 # Pokemons    
-charmander = Pokemon("Charmander", 5, "Fire", 100, 100, False)
-bulbasaur = Pokemon("Bulbasaur", 7, "Grass", 100, 100, False)
-psyduck = Pokemon("Psyduck", 6, "Water", 100, 100, False)
-staryu = Pokemon("Staryu", 7, "Water", 100, 100, False)
-ponyta = Pokemon("Ponyta", 4, "Fire", 100, 90, False)
-oddish = Pokemon("Oddish", 6, "Grass", 100, 100, False)
+Char = Pokemon("Charmander", 10, "Fire", 100, 52, False)
+Bul = Pokemon("Bulbasaur", 14, "Grass", 100, 30, False)
+Psy = Pokemon("Psyduck", 12, "Water", 100, 40, False)
+Star = Pokemon("Staryu", 8, "Water", 100, 65, False)
+Pony = Pokemon("Ponyta", 8, "Fire", 100, 60, False)
+Odd = Pokemon("Oddish", 6, "Grass", 100, 80, False)
+Heat = Pokemon("Heatmor", 4, "Fire", 100, 100, False)
+Squi = Pokemon("Squirtle", 6, "Water", 100, 70, False)
+Vic = Pokemon("Victreebel", 5, "Grass", 100, 90, False)
 
-ash_poke = [charmander, bulbasaur, psyduck]
-misty_poke = [staryu, ponyta, oddish]
+all_poke = [Char, Bul, Psy, Star, Pony, Odd, Heat, Squi, Vic]
+
+p1_poke = random.sample(all_poke, 4)
+p2_poke = random.sample(all_poke, 4)
+p1_name = input("Hi, Player 1! What is your name: ")
+p2_name = input("Player 2, nice to meet you! Give me your name: ")
 
 #Trainers
-Ash = Trainer("Ash", ash_poke, 3, ash_poke[1])
-Misty = Trainer("Misty", misty_poke, 4, misty_poke[0])
+Player1 = Trainer(p1_name, p1_poke, 5, p1_poke[0])
+Player2 = Trainer(p2_name, p2_poke, 5, p2_poke[0])
+
+print(f"{p1_name}, {p2_name}, check out your stats!\n")
+print("-"*80)
+print(Player1)
+print("-"*80)
+print(Player2)
+
+
+
+
+
+
+
